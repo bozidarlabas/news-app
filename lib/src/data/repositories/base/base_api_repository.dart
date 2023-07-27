7895394b2e48f5788fc7bb2082a0641ece2d7d01
Bozidar <bozidar@inquis-it.hr>
2023-07-27 09:40:38 +0200

    Implemented ApiRepositoryImpl

---
+++ lib/src/data/repositories/base/base_api_repository.dart
@@ -0,0 +1,27 @@
+import 'dart:io' show HttpStatus;
+import 'package:dio/dio.dart';
+import 'package:retrofit/dio.dart';
+import 'package:meta/meta.dart';
+
+import '../../../utils/resources/data_state.dart';
+
+abstract class BaseApiRepository {
+  @protected
+  Future<DataState<T>> getStateOf<T>({
+    required Future<HttpResponse<T>> Function() request,
+  }) async {
+    try {
+      final httpResponse = await request();
+      if (httpResponse.response.statusCode == HttpStatus.ok) {
+        return DataSuccess(httpResponse.data);
+      } else {
+        throw DioError(
+          response: httpResponse.response,
+          requestOptions: httpResponse.response.requestOptions,
+        );
+      }
+    } on DioError catch (error) {
+      return DataFailed(error);
+    }
+  }
+}
