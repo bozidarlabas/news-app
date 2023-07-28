223241029ccc92cd5ce1f294468e8fd05136691d
Bozidar <bozidar@inquis-it.hr>
2023-07-28 08:23:38 +0200

    Register database in dependency injection

--- lib/src/locator.dart
+++ lib/src/locator.dart
@@ -2,13 +2,24 @@ import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 import 'package:awesome_dio_interceptor/awesome_dio_interceptor.dart';
 
+import 'data/datasources/local/app_database.dart';
 import 'data/datasources/remote/news_api_service.dart';
 import 'data/repositories/api_repository_impl.dart';
+import 'data/repositories/database_repository_impl.dart';
 import 'domain/repositories/api_repository.dart';
+import 'domain/repositories/database_repository.dart';
+import 'utils/constants/strings.dart';
 
 final locator = GetIt.instance;
 
 Future<void> initializeDependencies() async {
+  final db = await $FloorAppDatabase.databaseBuilder(databaseName).build();
+  locator.registerSingleton<AppDatabase>(db);
+
+  locator.registerSingleton<DatabaseRepository>(
+    DatabaseRepositoryImpl(locator<AppDatabase>()),
+  );
+
   final dio = Dio();
   dio.interceptors.add(AwesomeDioInterceptor());
 
